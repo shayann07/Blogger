67950156fd1cb97f1319cacec22985d8aee11f92
Shayan Khan <shayankhan17353@gmail.com>
2025-03-22 02:29:21 +0500

    Implemented Model-BlogPost

--- app/src/main/java/com/example/securebloggingapp/model/BlogPost.java
+++ app/src/main/java/com/example/securebloggingapp/model/BlogPost.java
@@ -1,4 +1,62 @@
 package com.example.securebloggingapp.model;
 
 public class BlogPost {
+
+    private int id;
+    private String title;
+    private String content;
+    private String imagePath;
+    private String dateTime;
+
+    public BlogPost(int id, String title, String content, String imagePath, String dateTime) {
+
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.imagePath = imagePath;
+        this.dateTime = dateTime;
+
+    }
+
+    // Getters and Setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(String dateTime) {
+        this.dateTime = dateTime;
+    }
 }
