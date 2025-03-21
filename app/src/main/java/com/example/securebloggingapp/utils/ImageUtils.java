a4c10cec03cf24aea03742f542ab30675c0b07b1
Shayan Khan <shayankhan17353@gmail.com>
2025-03-22 03:21:15 +0500

    utils

--- app/src/main/java/com/example/securebloggingapp/utils/ImageUtils.java
+++ app/src/main/java/com/example/securebloggingapp/utils/ImageUtils.java
@@ -1,4 +1,23 @@
 package com.example.securebloggingapp.utils;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
+
+import java.io.IOException;
+
 public class ImageUtils {
+
+    public static final int PICK_IMAGE = 100;
+
+    public static void pickImageFromGallery(Activity activity) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        activity.startActivityForResult(intent, PICK_IMAGE);
+    }
+
+    public static Bitmap getBitmapFromUri(Activity activity, Uri uri) throws IOException {
+        return MediaStore.Images.Media.getBitmap(activity.getContentResolver(), uri);
+    }
 }
